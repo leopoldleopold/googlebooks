a3b37bd9c425b9677509abf437b392e16e10275c
Leopold Acosta <leopoldacosta@Leopolds-iMac.local>
2020-05-12 15:15:46 -0400

    added initial files

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/Navbar.css";
+
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+            <h1>GOOGLE BOOKS SEARCH</h1>
+        </nav>
+    );
+}
+
+export default Navbar;
