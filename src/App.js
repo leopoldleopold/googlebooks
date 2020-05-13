48bda0c0ba8541994533259253bbd7a1f4b674a5
Leopold Acosta <leopoldacosta@Leopolds-iMac.local>
2020-05-13 12:07:46 -0400

    added search styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar/Navbar";
+import Jumbotron from "./components/Jumbotron/Jumbotron";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Jumbotron />
     </div>
   );
 }
